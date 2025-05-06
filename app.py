23719e276431b4aeb44e4e415516206b7d1fd033
RenVit318 <79989306+RenVit318@users.noreply.github.com>
2025-05-06 15:02:55 +0200

    Setup main screen

--- app.py
+++ app.py
@@ -1,1 +1,42 @@
+import streamlit as st
+from FDP_handling import getFDPs
+from Query_handling import executeQuery
 
+
+STANDARD_QUERY = """ SELECT *
+WHERE {
+  ?s ?p ?o
+} LIMIT 10"""
+
+
+def setupContent():
+  st.title("FAIR Hackathon Query App")
+  st.write("Welcome to the web app to send a SPARQL query to one or more FAIR Data Points and to a triple store connected to it (if any).")
+  st.write("Below you can insert the URLs of the FAIR Data Points you want to include in your query and define your query.")
+
+  st.divider()
+
+  st.header("SPARQL Query")
+  
+  st.subheader("FAIR Data Points")
+  fdp_uris = getFDPs()
+  for fdp_uri in fdp_uris:
+    st.write(fdp_uri)
+
+
+  st.subheader("Query")
+  query = st.text_area(label="Type your SPARQL query here", placeholder=STANDARD_QUERY)
+  if st.button("Execute Query"):
+    executeQuery()
+  
+
+
+
+
+def main():
+  setupContent()
+
+
+
+if __name__ == '__main__':
+  main()

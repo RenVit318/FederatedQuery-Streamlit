2d603b2d85dec333ee61016d46059cf908a50a6d
RenVit318 <rens@renskievit.com>
2025-05-09 11:11:48 +0200

    Add messages for tracking

--- Query_handling.py
+++ Query_handling.py
@@ -6,7 +6,9 @@ def executeQuery(query):
     """Send the query over https, the alternative is using rdflib and injecting a SERVICE statement. 
     TODO: Consider pros and cons of these approaches"""
     for url in st.session_state.datasets:
+      st.info('Sending the query to ' + url)
       response = requests.post(url, data={'query': query})
+      st.info('Succesfully sent and received query, response: ' + response)
 
       
     st.write(response.text)
